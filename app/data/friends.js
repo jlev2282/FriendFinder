54680d383234d3d3fe2b797e2500865c25fffcc2
John <jlev2282@gmail.com>
2017-07-29 15:20:46 -0400

    routine push

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,27 +4,27 @@ var friends = [
     {
         name:"Emmitt",
         photo:"http://www.emmittsmith.com/sites/default/files/Emmitt_Smith_Dallas_Cowboys.jpg?1323053535",
-        scores:
+        scores: ["3","5","4","1","5","5","4","3","5","4"]
     },
     {
         name:"Barry",
         photo:"http://a.espncdn.com/photo/2013/0423/nfl_sanders_05.jpg",
-        scores:
+        scores:["4","5","2","5","2","5","1","4","5","4"]
     },
     {
         name:"Jim",
         photo:"http://media.cleveland.com/datacentral/photo/jim-brown-1958-training-campjpg-607b142f8d748ca9_medium.jpg",
-        scores:
+        scores:["2","5","2","5","5","5","4","5","5","4"]
     },
     {
         name:"Ricky",
         photo:"http://www.trbimg.com/img-579089f1/turbine/sfl-ricky-williams-name-change-20160721",
-        scores:
+        scores:["3","5","3","4","5","3","4","5","5","4"]
     },
     {
         name:"Marshawn",
         photo:"http://www.mercurynews.com/wp-content/uploads/2017/03/ap_570051645292.jpg?w=512",
-        scores:
+        scores:["3","5","4","1","2","5","4","3","5","4"]
     }
 
 ];
